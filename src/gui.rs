a9d19adfd0f5346f80ef988d59209c32cc2a1b88
Brewster Malevich <sbmalev@gmail.com>
2025-06-12 21:30:48 -0700

    Add mouse cursor, tooltips

--- src/gui.rs
+++ src/gui.rs
@@ -1,7 +1,7 @@
 //! Logic for the GUI.
 
-use crate::{CombatStats, GameState, Player, gamelog::GameLog};
-use rltk::{Console, RGB, Rltk};
+use crate::{CombatStats, GameState, Map, Name, Player, Position, gamelog::GameLog};
+use rltk::{Console, MAGENTA, Point, RGB, Rltk};
 use specs::prelude::*;
 
 /// Draws the GUI on screen.
@@ -47,5 +47,107 @@ pub fn draw_ui(ecs: &World, ctx: &mut Rltk) {
             ctx.print(2, y, s);
         }
         y += 1;
+    }
+
+    // Draw mouse cursor.
+    let mouse_pos = ctx.mouse_pos();
+    ctx.set_bg(mouse_pos.0, mouse_pos.1, RGB::named(rltk::MAGENTA));
+
+    draw_tooltips(ecs, ctx);
+}
+
+fn draw_tooltips(ecs: &World, ctx: &mut Rltk) {
+    // Get read access to needed components.
+    let map = ecs.fetch::<Map>();
+    let names = ecs.read_storage::<Name>();
+    let positions = ecs.read_storage::<Position>();
+
+    // Confirm mouse cursor is on the drawn map. Return if it isn't.
+    let mouse_pos = ctx.mouse_pos();
+    if mouse_pos.0 >= map.width || mouse_pos.1 >= map.height {
+        return;
+    }
+
+    // Print tool tip to UI. If mouse position is on the left put the tip on the right, and vice versa.
+    let mut tooltip: Vec<String> = Vec::new();
+    for (name, position) in (&names, &positions).join() {
+        let idx = map.xy_idx(position.x, position.y);
+        if position.x == mouse_pos.0 && position.y == mouse_pos.1 && map.visible_tiles[idx] {
+            tooltip.push(name.name.to_string());
+        }
+    }
+
+    if !tooltip.is_empty() {
+        let mut width: i32 = 0;
+        for s in tooltip.iter() {
+            if width < s.len() as i32 {
+                width = s.len() as i32;
+            }
+        }
+        width += 3;
+
+        if mouse_pos.0 > 40 {
+            let arrow_pos = Point::new(mouse_pos.0 - 2, mouse_pos.1);
+            let left_x = mouse_pos.0 - width;
+            let mut y = mouse_pos.1;
+            for s in tooltip.iter() {
+                ctx.print_color(
+                    left_x,
+                    y,
+                    RGB::named(rltk::WHITE),
+                    RGB::named(rltk::GREY),
+                    s,
+                );
+                let padding = (width - s.len() as i32) - 1;
+                for i in 0..padding {
+                    ctx.print_color(
+                        arrow_pos.x - i,
+                        y,
+                        RGB::named(rltk::WHITE),
+                        RGB::named(rltk::GREY),
+                        &" ".to_string(),
+                    );
+                }
+                y += 1;
+            }
+            ctx.print_color(
+                arrow_pos.x,
+                arrow_pos.y,
+                RGB::named(rltk::WHITE),
+                RGB::named(rltk::GREY),
+                &"->".to_string(),
+            );
+        } else {
+            let arrow_pos = Point::new(mouse_pos.0 + 1, mouse_pos.1);
+            let left_x = mouse_pos.0 + 3;
+            let mut y = mouse_pos.1;
+            for s in tooltip.iter() {
+                ctx.print_color(
+                    left_x + 1,
+                    y,
+                    RGB::named(rltk::WHITE),
+                    RGB::named(rltk::GREY),
+                    s,
+                );
+                let padding = (width - s.len() as i32) - 1;
+                for i in 0..padding {
+                    ctx.print_color(
+                        arrow_pos.x + 1 + i,
+                        y,
+                        RGB::named(rltk::WHITE),
+                        RGB::named(rltk::GREY),
+                        &" ".to_string(),
+                    );
+                }
+                y += 1;
+            }
+            ctx.print_color(
+                arrow_pos.x,
+                arrow_pos.y,
+                RGB::named(rltk::WHITE),
+                RGB::named(rltk::GREY),
+                &"<-".to_string(),
+            );
+        }
     }
 }
